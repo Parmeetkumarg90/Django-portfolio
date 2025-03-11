788cd9942501bb4147afead913efc42f528efd51
Parmeet Kumar <parmeetkumarg90@gmail.com>
2025-03-11 19:48:23 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 # Use Python 3 explicitly
-pip install --upgrade pip
-pip install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3.10 manage.py collectstatic --noinput
 
 # Run database migrations
-python manage.py migrate --noinput
+python3.10 manage.py migrate --noinput
