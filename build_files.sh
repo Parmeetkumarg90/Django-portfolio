12f4066648263745537812952fc32e58e465eac0
Parmeet Kumar <parmeetkumarg90@gmail.com>
2025-03-11 20:56:00 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,5 @@ pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
 
 # Run database migrations
-python3 manage.py makemigration --noinput
+python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
