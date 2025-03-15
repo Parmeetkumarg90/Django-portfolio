fa34e93a4c1ed198c6db4e0eff6604ad88512bdf
Parmeet Kumar <parmeetkumarg90@gmail.com>
2025-03-15 19:15:06 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from dotenv import load_dotenv
 from pathlib import Path
 import os
-import pymysql
-pymysql.install_as_MySQLdb()
+import pymysql  #vercel
+pymysql.install_as_MySQLdb() #vercel
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
